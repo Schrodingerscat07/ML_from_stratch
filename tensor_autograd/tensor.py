46957654bf69f702d09c4c8354516eed85d20060
Schrodingerscat07 <schrodingerscatwasmudered@gmail.com>
2026-01-08 22:35:55 +0530

    Enhance Tensor engine with matmul and transpose support

--- tensor_autograd/tensor.py
+++ tensor_autograd/tensor.py
@@ -12,6 +12,51 @@ class Tensor:
     def __repr__(self):
         return f"Tensor(data={self.data}, grad={self.grad})"
 
+    def __add__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data + other.data, (self, other), '+')
+
+        def _backward():
+            self.grad += out.grad
+            other.grad += out.grad
+        out._backward = _backward
+        return out
+
+    def __mul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data * other.data, (self, other), '*')
+
+        def _backward():
+            self.grad += other.data * out.grad
+            other.grad += self.data * out.grad
+        out._backward = _backward
+        return out
+
+    def __matmul__(self, other):
+        other = other if isinstance(other, Tensor) else Tensor(other)
+        out = Tensor(self.data @ other.data, (self, other), '@')
+
+        def _backward():
+            # Gradient for matrix multiplication:
+            # dL/dA = dL/dC @ B^T
+            # dL/dB = A^T @ dL/dC
+            self.grad += out.grad @ other.data.T
+            other.grad += self.data.T @ out.grad
+        out._backward = _backward
+        return out
+
+    def transpose(self):
+        out = Tensor(self.data.T, (self,), 'T')
+
+        def _backward():
+            self.grad += out.grad.T
+        out._backward = _backward
+        return out
+
+    @property
+    def T(self):
+        return self.transpose()
+
     def backward(self):
         # Topological sort for tensor graph
         topo = []
@@ -25,7 +70,6 @@ class Tensor:
         build_topo(self)
 
         # Reset gradients might be handled differently in complex scenarios,
-        # but for simplicity, we follow the scalar logic here:
         self.grad = np.ones_like(self.data, dtype=np.float64)
         for node in reversed(topo):
             node._backward()
